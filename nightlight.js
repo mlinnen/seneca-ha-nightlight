ab048a8a7124a776e2c6fa33f64bf7092b95bdab
Mike Linnen <mlinnen@protosystem.net>
2016-03-26 09:16:32 -0400

    Changed to call the suncalculator service every 30 seconds

--- nightlight.js
+++ nightlight.js
@@ -1,21 +1,26 @@
 
 
 module.exports = function nightlight(options) {
-
-  this.add({role:'suncalculator',event:'sunrise'}, function (msg, respond) {
-      console.log('Lights out')
-      this.act({role:'zwave',cmd:'control_off',id:7});  // 1st Floor Hall
-      this.act({role:'zwave',cmd:'control_off',id:8});  // 2nd Floor Hall
-      this.act({role:'zwave',cmd:'control_off',id:3});  // Family Room Lamp
-      this.act({role:'zwave',cmd:'control_off',id:10}); // Back Porch
-      this.act({role:'zwave',cmd:'control_off',id:4});  // Front Porch
-      respond(null, {answer: 'ok'})
-  })
-  this.add({role:'suncalculator',event:'sunset'}, function (msg, respond) {
-      console.log('Lights on')
-      this.act({role:'zwave',cmd:'control_on',id:4});  // Front Porch
-      respond(null, {answer: 'ok'})
-  })
-
+    var seneca = this;
+    
+    setInterval(function(options) {
+        seneca.act({ role: 'suncalculator', cmd: 'eventcheck', lat: 35.227085, long: -80.843124 }, function(err, result) {
+            if (result.answer=='none'){
+                console.log('not time yet');
+            } 
+            else if (result.answer=='sunrise'){
+                console.log('Lights out')
+                this.act({ role: 'zwave', cmd: 'control_off', id: 7 });  // 1st Floor Hall
+                this.act({ role: 'zwave', cmd: 'control_off', id: 8 });  // 2nd Floor Hall
+                this.act({ role: 'zwave', cmd: 'control_off', id: 3 });  // Family Room Lamp
+                this.act({ role: 'zwave', cmd: 'control_off', id: 10 }); // Back Porch
+                this.act({ role: 'zwave', cmd: 'control_off', id: 4 });  // Front Porch
+            }
+            else if (result.answer=='sunset'){
+                console.log('Lights on')
+                this.act({ role: 'zwave', cmd: 'control_on', id: 4 });  // Front Porch
+            }
+        })
+    }, 30000);
 }
 
