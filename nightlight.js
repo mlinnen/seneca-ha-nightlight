d5b4b658d08851efc645fd19d7954791e14db2c5
Mike Linnen <mlinnen@protosystem.net>
2016-03-27 12:07:08 -0400

    changed console log to seneca log

--- nightlight.js
+++ nightlight.js
@@ -6,10 +6,10 @@ module.exports = function nightlight(options) {
     setInterval(function(options) {
         seneca.act({ role: 'suncalculator', cmd: 'eventcheck', lat: 35.227085, long: -80.843124 }, function(err, result) {
             if (result.answer=='none'){
-                console.log.debug('not time yet');
+                seneca.log.debug('not time yet');
             } 
             else if (result.answer=='sunrise'){
-                console.log('Lights out')
+                seneca.log.info('Lights out')
                 this.act({ role: 'zwave', cmd: 'control_off', id: 7 });  // 1st Floor Hall
                 this.act({ role: 'zwave', cmd: 'control_off', id: 8 });  // 2nd Floor Hall
                 this.act({ role: 'zwave', cmd: 'control_off', id: 3 });  // Family Room Lamp
@@ -17,7 +17,7 @@ module.exports = function nightlight(options) {
                 this.act({ role: 'zwave', cmd: 'control_off', id: 4 });  // Front Porch
             }
             else if (result.answer=='sunset'){
-                console.log('Lights on')
+                seneca.log.info('Lights on')
                 this.act({ role: 'zwave', cmd: 'control_on', id: 4 });  // Front Porch
             }
         })
