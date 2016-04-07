e0ac96851c3c76c86cf3e72fcd1d98c0bc744b99
Mike Linnen <mlinnen@protosystem.net>
2016-04-06 21:21:41 -0400

    added the seneca-suncalculator dependency

--- startallservices.js
+++ startallservices.js
@@ -3,6 +3,6 @@ var seneca = require('seneca')()
 
 seneca.use('nightlight');
 seneca.use('../seneca-zwave-homegenie/zwave-control');
-seneca.use('../seneca-suncalculator/suncalculator');
+seneca.use('seneca-suncalculator');
 
 seneca.listen({host:"localhost" })
